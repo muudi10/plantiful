400dae70e64c0c42c131ed5b7f7974497a2de0fe
mahmoud jeilani <jeilani@gmail.com>
2021-11-20 16:02:21 +0000

    seperated the frontend folders from backend folders

--- API/data/mainApi.js
+++ API/data/mainApi.js
@@ -1,14 +1,11 @@
 const axios = require('axios') ;
 require('dotenv').config()
-const urlEndPoint = process.env.APIENDPOINT
+const APIENDPOINT = process.env.APIENDPOINT
 const getData = async() => {
-    const plants = await axios.get(`https://f7tjuu3dc1.execute-api.us-east-1.amazonaws.com/test/plants`,
+    const plants = await axios.get(APIENDPOINT,
         )
      const data = await plants.data
      console.log(data)
-
-
-
 }
 
 getData()
